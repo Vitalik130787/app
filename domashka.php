f272e7a33ff0016509d7fd3c023ca28a218464e2
Vitalik <sensey130787@gmail.com>
2021-12-13 11:47:33 +0200

    111

---
+++ domashka.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Домашня робота</title>
+</head>
+<body>
+    <h1>Домашка</h1>    
+<?php
+    $a=20;
+    $GL ="privet";
+    $_RE = $a + $GL;
+   echo($_RE);
+?>
+<?php
+
+?>
+
+<form action="/public/example1.php" method="post">
+    Ваше ім'я: <input type="text" name="name"><br>
+    Ваше ім'я: <input type="text" name="name"><br>
+    <input type="submit">
+</form>
+</body>
+</html>
